a1affe3394aaad97af1ea86129df0eca9c970abe
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-12-03 16:14:46 +0300

    Pep-8

--- string_reverse.py
+++ string_reverse.py
@@ -1,17 +1,18 @@
 import sys
 
+
 def string_reverse(input_string):
-	result = ''
-	for i in input_string:
-		result = i + result
-	return result
+    result = ''
+    for i in input_string:
+        result = i + result
+    return result
 
 if __name__ == '__main__':
-	if len(sys.argv)<2:
-		sys.stderr.write('This util needs some input: ./string_reverse.py string_to_reverse\n')
-		sys.exit()
-	input_string = sys.argv[1]
-	if len(sys.argv)>2:
-		sys.stderr.write('Warning: the second and the following arguments ignored\n')
-	sys.stdout.write('Initial string: {}\n'.format(input_string))
-	sys.stdout.write('Reversed string: {}\n'.format(string_reverse(input_string)))
+    if len(sys.argv) < 2:
+        sys.stderr.write('This util needs some input: ./string_reverse.py string_to_reverse\n')
+        sys.exit()
+    input_string = sys.argv[1]
+    if len(sys.argv) > 2:
+        sys.stderr.write('Warning: the second and the following arguments ignored\n')
+    sys.stdout.write('Initial string: {}\n'.format(input_string))
+    sys.stdout.write('Reversed string: {}\n'.format(string_reverse(input_string)))
